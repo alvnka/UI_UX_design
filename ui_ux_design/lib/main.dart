db27a426d6feda41a3e036cef2877c66aa1bb1ae
alvnka <alvoausto@gmail.com>
2023-03-06 22:53:27 +0300

    merged changes

--- ui_ux_design/lib/main.dart
+++ ui_ux_design/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: ' Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -100,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
--- ui_ux_design/ui lazer/ui_ux_design/android/app/src/main/kotlin/com/example/ui_ux_design/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ui_ux_design
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
